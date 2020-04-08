f9249897c4face13ad7ac08c1a17c6f68ffd99ed
eadortsu <mail@eadortsu.com>
2020-04-08 17:35:46 +0000

    Challenge 1 to 3

--- src/estimator.php
+++ src/estimator.php
@@ -2,5 +2,89 @@
 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+    $input = json_decode($data);
+    $impact = new stdClass();
+    $severImpact = new stdClass();
+    $output = new stdClass();
+
+
+    // Calculate impact.currentlyInfected
+    $impact->currentlyInfected = $input->reportedCases * 10;
+
+    // Calculate severImpact.currentlyInfected
+    $severImpact->currentlyInfected = $input->reportedCases * 50;
+
+    // Calculate infectionsByRequestedTime
+    $duration = normaliseDuration($input->periodType, $input->timeToElapse);
+    $factor = intval($duration / 3);
+    $impact->infectionsByRequestedTime = $impact->currentlyInfected * (pow(2, $factor));
+    $severImpact->infectionsByRequestedTime = $severImpact->currentlyInfected * (pow(2, $factor));
+
+    // Challenge 2
+    // Calculate severeCasesByRequestedTime
+    $impact->severeCasesByRequestedTime = $impact->infectionsByRequestedTime * (15 / 100);
+    $severImpact->severeCasesByRequestedTime = $severImpact->infectionsByRequestedTime * (15 / 100);
+
+    // Calculate hospitalBedsByRequestedTime
+    $availableBed = $input->totalHospitalBeds * (35 / 100);
+    $impact->hospitalBedsByRequestedTime = $availableBed - $impact->severeCasesByRequestedTime;
+    $severImpact->hospitalBedsByRequestedTime = $availableBed - $severImpact->severeCasesByRequestedTime;
+
+    // Challenge 3
+    $impact->casesForICUByRequestedTime = $impact->infectionsByRequestedTime * (5 / 100);
+    $severImpact->casesForICUByRequestedTime = $severImpact->infectionsByRequestedTime * (5 / 100);
+
+    //  casesForVentilatorsByRequestedTime
+    $impact->casesForVentilatorsByRequestedTime = $impact->infectionsByRequestedTime * (2 / 100);
+    $severImpact->casesForVentilatorsByRequestedTime = $severImpact->infectionsByRequestedTime * (2 / 100);
+
+    // dollarsInFlight
+    $impact->dollarsInFlight = $impact->infectionsByRequestedTime * $input->region->avgDailyIncomeInUSD * $input->region->avgDailyIncomePopulation;
+    $severImpact->dollarsInFlight = $severImpact->infectionsByRequestedTime * $input->region->avgDailyIncomeInUSD * $input->region->avgDailyIncomePopulation;
+
+    $output->data = $input; // the input data you got
+    $output->impact = $impact; // your best case estimation
+    $output->severeImpact = $severImpact; // your severe case estimation
+    return json_encode($output);
 }
+
+function normaliseDuration($periodType, $timeToElapse)
+{
+    $days = 0;
+
+    switch ($periodType) {
+        case "days":
+            $days = $timeToElapse;
+            break;
+        case "weeks":
+            $days = 7 * $timeToElapse;
+            break;
+        case "months":
+            $days = 30 * $timeToElapse;
+            break;
+        case "years":
+            $days = 365 * $timeToElapse;
+            break;
+        default:
+            break;
+    }
+
+    return $days;
+}
+
+
+$data = '{
+    "region": {
+        "name": "Africa",
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 5,
+        "avgDailyIncomePopulation": 0.71
+    },
+    "periodType": "days",
+    "timeToElapse": 58,
+    "reportedCases": 674,
+    "population": 66622705,
+    "totalHospitalBeds": 1380614
+}';
+
+echo(covid19ImpactEstimator($data));
