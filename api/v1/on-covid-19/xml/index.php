d6e34d445f309194e2b4e78ba5e308d8b8cf887b
eadortsu <mail@eadortsu.com>
2020-04-13 15:05:50 +0000

    API setup

--- api/v1/on-covid-19/xml/index.php
+++ api/v1/on-covid-19/xml/index.php
@@ -16,9 +16,9 @@ $input = (array)json_decode(file_get_contents('php://input'), TRUE);
     exit();
 }*/
 
-/*if (!validatePerson($input)) {
+if (!validatePerson($input)) {
     return unprocessableEntityResponse();
-}*/
+}
 
 header('HTTP/1.1 200 Ok');
 
